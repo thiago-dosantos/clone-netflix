013e3dbdcf74f9bf2cb8ea95c76ec780da436c3b
Thiago dos Santos <90911509+thiago-dosantos@users.noreply.github.com>
2023-06-19 11:28:52 -0300

    Clone Netflix
    Project in Next JS with TS and tailwind, where the content of the netflix catalog is presented containing techniques in CSS, react libraries, etc.

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='flex items-center justify-center bg-zinc-900 mt-8 h-[80px] bottom-0 w-full'>© All image rights are reserved by<a href='https://netflix.com/'>&nbsp;Netflix</a></div>
+  )
+}
+
+export default Footer;
---
+++ components/IndexPage.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+ 
+function IndexPage() {
+  return (
+    <div>
+      <Head>
+        <title>Clone Netflix</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" type='image/x-icon' href="https://cdn.icon-icons.com/icons2/2699/PNG/512/netflix_logo_icon_170919.png" sizes="any" />
+      </Head>
+    </div>
+  )
+}
+ 
+export default IndexPage
